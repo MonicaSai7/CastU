914f81e2c9f8ce6eca277b7c881f39fb10a5b622
MonicaSai7 <monica.sai7@gmail.com>
2021-06-13 22:33:50 +0530

    Add auth controllers

--- auth/auth.py
+++ auth/auth.py
@@ -3,12 +3,11 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
-
-AUTH0_DOMAIN = 'cafe-sky.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'cafe'
-CLIENT_ID = 'MM2K8zHkJvc2PTqtVeKVyJy2hp8l6sVP'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']#'castu.us.auth0.com'
+ALGORITHMS = os.environ['ALGORITHMS']#['RS256']
+API_AUDIENCE = os.environ['API_AUDIENCE']#'agency'
 
 ## AuthError Exception
 '''
@@ -182,6 +181,7 @@ def requires_auth(permission=''):
                 payload = verify_decode_jwt(token)
                 check_permissions(permission, payload)
             except Exception as e:
+                print(e)
                 abort(401)
             
             return f(payload, *args, **kwargs)
